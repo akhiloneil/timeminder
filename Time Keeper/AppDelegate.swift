65ef01fd5731ed76cc1c1357e7339b05343169e1
Akhil Sharma <akhilspc@Akhils-Mac-mini.local>
2017-07-27 12:28:51 -0700

    w

--- Time Keeper/AppDelegate.swift
+++ Time Keeper/AppDelegate.swift
@@ -10,17 +10,27 @@ import UIKit
 import UserNotifications
 import Foundation
 
-var launch: Bool = false 
-
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
     var window: UIWindow?
-    let center = UNUserNotificationCenter.current()
+let center = UNUserNotificationCenter.current()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        /*let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound, .badge]) { (granted,error:Error?) in
+            // Enable or disable features based on authorization
+            if !granted {
+                print("App is useless because you didn't allow notifications. Please go to settings and allow all notifications")
+            }
+        }*/
+        //let notificationSettings = UNNotificationSettings(forTypes: [.badge, .alert, .sound], categories:nil)
+        //application.registerUserNotificationSettings(notificationSettings)
         
-                        return true
+                application.beginBackgroundTask(withName: "showNotification", expirationHandler: nil)
+        
+                return true
     } 
 
     func applicationWillResignActive(_ application: UIApplication) {
@@ -33,23 +43,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-        //print(userSettings.counter)
-        //print(userSettings.repeatCount) // For Debugging. Global var for the repeat count on the timer in the pacer view controller
-        
-        //print(CACurrentMediaTime())
-        /*let x = UIBackgroundTaskIdentifier()
+        print(userSettings.counter)
+        print(userSettings.repeatCount) // For Debugging. Global var for the repeat count on the timer in the pacer view controller
+        let x = UIBackgroundTaskIdentifier()
         //if (userSettings.counter < userSettings.repeatCount) {
             //print("made it here into background mode")
             var timer = Timer()
-            if !timer.isValid {
-                    timer = Timer.scheduledTimer(withTimeInterval: 5, repeats: false, block: { (timer) in
+            if !timer.isValid && (userSettings.counter < userSettings.repeatCount) {
+                    timer = Timer.scheduledTimer(withTimeInterval: 100, repeats: false, block: { (timer) in
                         application.endBackgroundTask(x)
-                        application.beginBackgroundTask(withName: "showAlert", expirationHandler: nil)
-                        print("made it in the background renewal")
+                        application.beginBackgroundTask(withName: "showNotification", expirationHandler: nil)
+                
                 
                 
                     })
-        }*/
+        }
+
+        
+        else {
+            application.endBackgroundTask(x)
+            print("ended background task because the timer was up")
+        }
         
         
     }
@@ -57,26 +71,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-        if UIApplication.shared.currentUserNotificationSettings?.types == .none {
-            // not registered
-            userSettings.isGrantedAccess = false
-            print("user didn't allow notifications")
-        } else {
-            // registered in some way
-            userSettings.isGrantedAccess = true
-        }
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-        if UIApplication.shared.currentUserNotificationSettings?.types == .none {
-            // not registered
-            userSettings.isGrantedAccess = false
-            print("user didn't allow notifications")
-        } else {
-            // registered in some way
-            userSettings.isGrantedAccess = true
-        }
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
